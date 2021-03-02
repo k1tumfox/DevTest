997583b8c906adb8689c02f02fd64a1e0b486485
Tenyson Kwon <tenysonkwon@gmail.com>
2021-03-01 20:03:30 -0500

    q2 done

--- question2/main.ts
+++ question2/main.ts
@@ -1,3 +1,21 @@
-export function findOutlier(integers: number[]): number {
-    
+export function findOutlier(integers: number[]) {
+  
+  if (integers.length >= 3) {
+    const evens = integers.filter(ele => ele%2 === 0);
+    const odds = integers.filter(ele => ele%2 !== 0);
+
+    if (evens.length > odds.length) {
+      return odds[0];
+    } else {
+      return evens[0];
+    }
+
+  } else {
+    return null;
   }
+}
+
+// notes:
+// - array of integers with min length 3
+// - array is either all odd or even except for a single int N
+// - return "outlier" N
